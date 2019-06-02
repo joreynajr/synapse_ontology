6ad72e58942e7de09c0cf029be8fa8e5b2e1a893
joreyna <joreyna@epiglass.ucsd.edu>
2019-06-01 23:14:59 -0700

    Annotated gene ontology

--- scripts/extract_num_clusters.sh
+++ scripts/extract_num_clusters.sh
@@ -2,7 +2,9 @@ echo "Alpha test:"
 fns=$(ls ../output/StringDB/string_synapse_interactions_combined_score.clixo_alpha*_beta0.5.txt)
 for fn in $fns; 
 	do 
+	echo $fn
 	grep "Num clusters" $fn; 
+	echo 
 	done
 
 echo
@@ -10,5 +12,7 @@ echo "Beta test:"
 fns=$(ls ../output/StringDB/string_synapse_interactions_combined_score.clixo_alpha0.5_beta*.txt)
 for fn in $fns; 
 	do 
+	echo $fn
 	grep "Num clusters" $fn; 
+	echo 
 	done
