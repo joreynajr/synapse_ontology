6330c1bf6a3474fe9b5118f2cf877faa0e1578ff
joreynajr <joreyna@live.com>
2019-06-07 17:33:26 -0700

    New Run_Clixo notebook created

--- scripts/Run_Clixo.sh
+++
@@ -1,8 +0,0 @@
-clixo_script="/gpfs/data01/glasslab/home/joreyna/projects/BNFO286/clixo_0.3/clixo"
-alpha=$1
-beta=$2
-interactions_fn="../output/StringDB/string_synapse_interactions_combined_score.tsv"
-clixo_fn="../output/StringDB/string_synapse_interactions_combined_score.clixo_alpha${alpha}_beta${beta}.txt"
-echo $clixo_fn
-cmd="$clixo_script $interactions_fn $alpha $beta > $clixo_fn"
-eval $cmd
